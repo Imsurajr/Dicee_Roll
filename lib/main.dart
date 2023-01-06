9285408318bd95cf525c48099c0e64c55ef405f6
Suraj Raghuvanshi <suraj.raghuvanshi2021@vitbhopal.ac.in>
2023-01-06 17:21:06 +0530

    Initial Commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.cyan.shade900,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          centerTitle: true,
+          title: Text('Dicee Roll'),
+          backgroundColor: Colors.blueGrey.shade600,
         ),
         body: DicePage(),
       ),
@@ -15,9 +18,69 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void leftChangeDiceFace() {
+    setState(
+      () {
+        print('Right button pressed ');
+        rightDiceNumber = Random().nextInt(6) + 1;
+      },
+    );
+  }
+
+  void rightChangeDiceFace() {
+    setState(
+      () {
+        print('Left button pressed');
+        leftDiceNumber = Random().nextInt(6) + 1;
+      },
+    );
+  }
+
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child:
+                // Image(
+                //   image: AssetImage('images/dice1.png'),
+                TextButton(
+              onPressed: () {
+                rightChangeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          // ),
+
+          Expanded(
+            child:
+                // Image(
+                //   image: AssetImage('images/dice1.png'),
+                TextButton(
+              onPressed: () {
+                leftChangeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+          // ),
+        ],
+      ),
+    );
   }
 }
